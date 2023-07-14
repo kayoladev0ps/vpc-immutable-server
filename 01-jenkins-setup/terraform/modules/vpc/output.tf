693c6ae56ba513032498fdff396568ed8b379c0d
kayoladevops <kay.ola@intuitiontrucking.com>
2023-07-14 03:12:05 -0700

    new changes

---
+++ 01-jenkins-setup/terraform/modules/vpc/output.tf
@@ -0,0 +1,51 @@
+output "region" {
+    value = var.region
+}
+
+output "project_name" {
+    value = var.project_name
+}
+
+output "environment" {
+    value = var.environment
+}
+
+output "vpc-id" {
+    value = aws_vpc.vpc.id
+}
+
+output "public_subnet_az1_id" {
+    value = aws_subnet.public_subnet_az1.id
+}
+
+output "public_subnet_az2_id" {
+    value = aws_subnet.public_subnet_az2.id
+}
+
+output "private_subnet_az1_id" {
+    value = aws_subnet.private_app_subnet_az1.id
+}
+
+output "private_subnet_az2_id" {
+    value = aws_subnet.private_app_subnet_az2.id
+}
+
+output "private_data_subnet_az1_id" {
+    value = aws_subnet.private_data_subnet_az1.id
+}
+
+output "private_data_subnet_az2_id" {
+    value = aws_subnet.private_data_subnet_az2.id
+}
+
+output "availability_zone_1" {
+    value = data.aws_availability_zones.available_zones.names[0]
+}
+
+output "availability_zone_2" {
+    value = data.aws_availability_zones.available_zones.names[1]
+}
+
+output "internet_gateway" {
+    value = aws_internet_gateway.internet_gateway
+}
